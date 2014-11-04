11c2463be15c4c3a9d7c3b5c5ec6214e7cf3f66a
Jboutte <jboutte@mail.csuchico.edu>
2014-11-04 11:39:42 -0800

    Fixes output formatting

--- hw7/k_near_neigh.py
+++ hw7/k_near_neigh.py
@@ -11,23 +11,26 @@ def import_matrix(file):
     matrix.append(row)
   return matrix
 
-def calc_euclidian(l1, l2):
-  value = 0
-  for i in range(len(l1)):
-    value += pow(l1[i] - l2[i], 2)
+def euclidean_distance(l1, l2):
+  value = 0 
+  for x in range(len(l1)):
+    value += pow(l1[x] - l2[x], 2)
   return math.sqrt(value)
+
 
 input_matrix = import_matrix('input_hw7.txt')
 class_labels = import_matrix('class_label_hw7.txt')
 query = [4, 3, 2]
 
-results = {}
+results = []
 
 for i in range(len(input_matrix)):
-  results[i] = calc_euclidian(input_matrix[i], query)
+  row = [i]
+  row.append(euclidean_distance(input_matrix[i], query))
+  row.append(class_labels[i][0]) 
+  results.append(row)
 
-sorted_results = sorted(results.items(), key=lambda x:x[1])
+results.sort(key = lambda row: row[1])
 
 for i in range(5):
-  print(str(sorted_results[i][1]) + ' ' + str(class_labels[sorted_results[i][0]][0]))
-
+  print('Value ' + str(results[i][1]) + ' Class ' + str(results[i][2]))
